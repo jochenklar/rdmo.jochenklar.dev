5bb2fac210b400e4a6dd7e9329677503e9452e07
Jochen Klar <mail@jochenklar.de>
2022-01-06 13:07:26 +0100

    Fix email config

--- config/settings/local.py
+++ config/settings/local.py
@@ -44,8 +44,8 @@ if os.getenv('EMAIL_HOST'):
     EMAIL_PORT = os.getenv('EMAIL_PORT')
     EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-    EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
-    EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
+    EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', '').upper() == 'TRUE'
+    EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', '').upper() == 'TRUE'
     DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 ACCOUNT = True
