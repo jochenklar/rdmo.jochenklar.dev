8a742ecb79dce5e0f2d4fbe6b6e41ce476195b71
Jochen Klar <mail@jochenklar.de>
2023-12-23 16:59:46 +0100

    Refactor settings

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,11 +1,8 @@
-from os import environ
 from pathlib import Path
 
 from split_settings.tools import include, optional
 
 from rdmo.core.settings import *
-
-DJANGO_ENV = environ.get('DJANGO_ENV', 'production')
 
 BASE_DIR = Path(__file__).parent.parent.parent
 MEDIA_ROOT = BASE_DIR / 'media_root'
@@ -14,6 +11,5 @@ STATICFILES_DIRS = [BASE_DIR / 'vendor']
 
 include(
     'base.py',
-    f'environments/{DJANGO_ENV}.py',
-    optional('local.py')
+    'local.py'
 )
